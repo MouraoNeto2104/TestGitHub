126cf93f9326c68191c90e660d0bb56995de490c
MouraoNeto2104 <fdmouraoneto@gmail.com>
2021-06-25 16:27:08 -0300

    Teste de software para verificar se numero e positivo ou nao.

---
+++ TesteGitHub01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TesteGitHub01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the whole number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if(number > 0)
+            {
+                Console.WriteLine("The number is positive!");
+            }
+            else
+            {
+                Console.WriteLine("The number is negative or null!");
+            }
+        }
+    }
+}
